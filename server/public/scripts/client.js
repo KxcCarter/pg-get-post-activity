ba1a505459cf7cb7951a33c06203c600e295e0d9
Kenneth Carter <kxccarter@gmail.com>
2020-07-08 16:17:04 -0500

    Implemented POST route in GUI.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,7 @@ $(document).ready(init);
 function init() {
     console.log(`we are so ready`);
     renderLibrary();
+    $('.js-add-book').on('submit', clickAddBook);
 }
 
 function renderLibrary() {
@@ -24,4 +25,32 @@ function renderLibrary() {
             `);
         }
     });
+}
+
+function clickAddBook(event) {
+    event.preventDefault();
+    console.log(`add book!`);
+    const newBookObject = {
+        title: $('#jsAddBookTitle').val(),
+        author: $('#jsAddBookAuthor').val(),
+        published: $('#jsAddBookPublished').val(),
+    };
+    console.log(newBookObject);
+
+    $('.js-add-book').trigger('reset');
+
+    $.ajax({
+            type: 'POST',
+            url: '/books',
+            data: newBookObject,
+        })
+        .then((response) => {
+            console.log(response);
+            renderLibrary();
+        })
+        .catch((err) => {
+            console.log(err);
+
+            console.log(`You done messed up, A-Aron!`);
+        });
 }
